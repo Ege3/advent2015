e929f25342cae7f4d921aeaf38e9a01c0609fb97
Ege3 <ege3@msn.com>
2019-12-11 14:22:09 +0200

    init

--- day1_1.php
+++ day1_1.php
@@ -1,2 +1,10 @@
 <?php
-echo "hello\n";
+$file = fopen("day1.txt", "r");
+$floors = fgets($file);
+fclose($file);
+
+$down = substr_count($floors, '(');
+$up = substr_count($floors, ')');
+
+echo ($down - $up) . "\n";
+
